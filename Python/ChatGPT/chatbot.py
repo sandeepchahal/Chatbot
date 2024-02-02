8a8add0ca953e6e206465f0e9846897472391430
sandeepsinghssn <ssn@leapwork.com>
2024-02-02 20:09:33 +0530

    created embedding from csv file

--- Python/ChatGPT/chatbot.py
+++ Python/ChatGPT/chatbot.py
@@ -18,17 +18,17 @@ file_directory_name = "content/"
 current_dir = os.path.dirname(os.path.abspath(__file__))
 content_directory = os.path.join(current_dir, file_directory_name)
 
-
-csv_file_name = 'patientInfo.csv'
 embedding_file_name = "embeddings.csv"
+csv_inputput_file_name = "patients.csv"
+
 not_applicable_response = "Sorry, I can only answer the questions that are relevant to the medical or health care"
 
 default_message = {
     "role": "system",
     "content": (
         "You are an assistant in a Apollo Hospital. "
-        "Answer the questions precisel by following below rules: \n"
-        "1. Evaluate the question, and if it doesn't pertain to subjects such as medicine, viruses, health, diseases, doctors, patients or hospitals, say'NAAA'. \n"
+        "Answer the questions precisely by following below rules: \n"
+        "1. Evaluate the question, and if it doesn't pertain to subjects such as medicine, viruses, health, diseases, doctors, patients information or hospitals, say'NAAA'. \n"
         "2. Only answer to valid questions. \n"
         "3. Do not justify your answers. \n"
         "4. Respond appropriately to greetings. \n"+
@@ -39,9 +39,10 @@ default_message = {
 default_context_message = {
     "role": "system",
     "content": (  
-        "6. Answer questions only based on the provided 'Context'. \n"
-        "7. If the given 'Context' does not provide any information about question, only say 'NF'. \n"
-        "8. Never provide information that is not mentioned in the given 'Context'. \n"
+        "6. The 'Context' is having the patients information such as name, address, illness, date,etc. You should answer correctly when asked about patients information"
+        "7. Answer questions only based on the provided 'Context'. \n"
+        "8. If the given 'Context' does not provide any information about question, only say 'NF'. \n"
+        "9. Never provide information that is not mentioned in the given 'Context'. \n"
     )
 }
 
@@ -56,25 +57,44 @@ OPEN_API_KEY = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=OPEN_API_KEY)
 
 def main():
-    global context_df
+    global context_df, columns_list
     ## load file into variable
+
+    ## creating embedding from CSV file
     if os.path.exists(content_directory + embedding_file_name) == False:
-        if embedding.createEmbedding(directory_path=content_directory, output_csv_path=content_directory + csv_file_name):
+        if embedding.create_embeding_csv_file(directory_path=content_directory, input_csv_filename=csv_inputput_file_name, output_embedding_filename=embedding_file_name):
             print("Embedding is created")
         else:
             print("Embedding not created")
     else:
         print("Embedding already exists")
+    
+    original_file = pd.read_csv(os.path.join(content_directory,csv_inputput_file_name )) ## original file with patient info
+    columns_list = original_file.columns.tolist()
+
+
+    # Creating embedding from text Files
+    ## Only 1 would work 
+    # if os.path.exists(content_directory +embedding_file_name ) == False:
+    #     if embedding.create_embedding_text_files(directory_path=content_directory, output_csv_filename=embedding_file_name):
+    #         print("Embedding is created")
+    #     else:
+    #         print("Embedding not created")
+    # else:
+    #     print("Embedding already exists")
         
     context_df = pd.read_csv(os.path.join(content_directory,embedding_file_name))
+
     context_df['embeddings'] = context_df['embeddings'].apply(ast.literal_eval)
-    
+
     demo = gr.ChatInterface(answer_question)
     demo.launch() 
 
 def search_response_from_embedding(user_input):
     print('seaching in Document')
+    global context_df
     embeddings_array = np.array(list(context_df['embeddings']))
+   
     question_embedding = client.embeddings.create(input=user_input, model='text-embedding-ada-002').data[0].embedding
 
     nn_model = NearestNeighbors(n_neighbors=5, metric='cosine')
@@ -84,12 +104,14 @@ def search_response_from_embedding(user_input):
     _, indices = nn_model.kneighbors([question_embedding])
 
     # Extract the relevant information from the DataFrame based on the indices
-    similar_responses_df = context_df.iloc[indices[0]]
+    similar_responses_df =  context_df.iloc[indices[0]]
 
     # Process the relevant information, e.g., extract the text or other attributes
     return process_similar_responses(similar_responses_df, 2000)
     
 def process_similar_responses(similar_responses_df, max_tokens):
+    print(similar_responses_df['text'].tolist())
+
     responses = similar_responses_df['text'].tolist()
     
     # Initialize variables
@@ -115,15 +137,21 @@ def process_similar_responses(similar_responses_df, max_tokens):
 def document_answer(question, history):
     try:
         # Create a chat comxpletion using the question and context
-        
+        global columns_list
         context = search_response_from_embedding(question)
+        print('context from document', context)
+
         final_context_reduced = reduce_context_size_if_needed(context)
         final_context = context_message.copy() # it will always keep the copy of default context along with context created from file
         if(len(history)>0):
             reduce_history_if_needed(history)
             final_context.extend(history)
-            
-        final_context.append({"role":"user", "content": f"Context: {final_context_reduced}\n\n---\n\nQuestion: {question}"})
+
+        if(len(columns_list)>0):
+            input_data = {'columns': columns_list, 'data': final_context_reduced}
+            final_context.append({"role":"user", "content": f"Context: {input_data}\n\n---\n\nQuestion: {question}"})
+        else:
+            final_context.append({"role":"user", "content": f"Context: {final_context_reduced}\n\n---\n\nQuestion: {question}"})
         
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
@@ -136,7 +164,7 @@ def document_answer(question, history):
         )
         return response.choices[0].message.content
     except Exception as e:
-        print('Error In Document Response \n',e)
+        #print('Error In Document Response \n',e)
         return "error" 
 
 def reduce_history_if_needed(history_context,max_tokens = 1500):
--- Python/ChatGPT/myEmbedding.py
+++ Python/ChatGPT/myEmbedding.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 import os
 import pandas as pd
 import tiktoken
+import numpy as np
 
 tokenizer = tiktoken.get_encoding("cl100k_base")
 pd.options.mode.copy_on_write = True
@@ -11,7 +12,7 @@ OPEN_API_KEY = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(
     api_key= OPEN_API_KEY
 )
-
+split_sentence_filename = 'sentences.csv'
 maximum_tokens = 300
 class MyEmbedding:
     
@@ -22,14 +23,14 @@ class MyEmbedding:
         serie = serie.str.replace('  ', ' ')
         return serie
     
-    def _process_files(self,directory_path, output_csv_path):
+    def _process_files(self,directory_path):
         ## this function will process all txt files and dump the data into csv by removing some extra spaces,etc.
         texts=[]
         text_files = [f for f in os.listdir(directory_path) if f.endswith('.txt')]
         for text_file in text_files:
             print(f"Processing the file -  {text_file}")
             file_path = os.path.join(directory_path, text_file)
-            with open(file_path, 'r') as file:
+            with open(file_path, 'r',encoding='utf-8') as file:
                 text = file.read()
                 texts.append(text)
 
@@ -37,7 +38,7 @@ class MyEmbedding:
         df = pd.DataFrame(texts, columns = ['text'])
         df['text'] = self._remove_newlines(df.text)
         df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
-        df.to_csv(output_csv_path)
+        df.to_csv(os.path.join(directory_path,split_sentence_filename))
         print("CSV file is created")
     
     def _split_sentence(self, row, max_tokens = maximum_tokens):
@@ -58,12 +59,12 @@ class MyEmbedding:
             tokens_so_far += token + 1
         return chunks
     
-    def createEmbedding(self,directory_path, output_csv_path,model="text-embedding-ada-002"):
+    def create_embedding_text_files(self,directory_path, output_csv_filename,model="text-embedding-ada-002"):
 
         try:
             ## 1. process the files into csv
-            self._process_files(directory_path=directory_path, output_csv_path= output_csv_path)
-            df = pd.read_csv(output_csv_path)
+            self._process_files(directory_path=directory_path)
+            df = pd.read_csv(os.path.join(directory_path,split_sentence_filename))
             df.head()
             ## 2. create tokens 
             ## check if row token in greater than max tokens
@@ -81,9 +82,28 @@ class MyEmbedding:
             print("Sentence split done")
 
             df['embeddings'] = df.text.apply(lambda x: client.embeddings.create(input=x,model=model).data[0].embedding)
-            df.to_csv(directory_path+"embeddings.csv")
+            df.to_csv(os.path.join(directory_path,output_csv_filename))
             return True
         except:
-             raise Exception("an error occurred")
-    
+            raise Exception("an error occurred")
+        
+    def create_embeding_csv_file(self, directory_path,input_csv_filename,output_embedding_filename, model ="text-embedding-ada-002" ):
+        try:
+            print(f'Reading from CSV file from {os.path.join(directory_path,input_csv_filename)} & Creating Embedding')
+            file_data = pd.read_csv(os.path.join(directory_path,input_csv_filename))
+            columns = file_data.columns.tolist()
+            combined_text =  file_data[columns].apply(lambda row: ' '.join(map(str, row)), axis=1)
+            df = pd.DataFrame(combined_text, columns = ['text'])
+            count =0
+            for text in df.text:
+                emb = client.embeddings.create(input=text,model=model).data[0].embedding
+                count += 1
+                print('Embedding done for - ',count)
+
+            df['embeddings'] = df.text.apply(lambda x: client.embeddings.create(input=x,model=model).data[0].embedding)
+            df.to_csv(directory_path+output_embedding_filename)
+            return True
+        except Exception as e:
+            print(e)
+            raise Exception("an error occurred")
     
